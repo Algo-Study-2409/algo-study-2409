aea96f23cad88d84912651925dfcb0614a5e8e7f
Chaeniiiii <kce2360@gmail.com>
2024-09-19 05:16:05 +0900

    강채은 : BOJ 1021 회전하는 큐

--- problems/week01/강채은/BOJ1021_회전하는 큐.java
+++ problems/week01/강채은/BOJ1021_회전하는 큐.java
@@ -30,14 +30,9 @@ public class Main {
 
         for(int num : want){
             
-            int index = deque.indexOf(num);
-            int mid = 0;
-
-            //짝수일 경우 -1 -> index는 0부터 시작
-            if(deque.size() % 2 == 0) mid = deque.size()/2-1;
-            else mid = deque.size()/2;
-
-            int element = 0;
+            int index = deque.indexOf(num); //뽑아 낼 수의 위치 
+            int mid = deque.size()/2; //뽑아 낼 수의 절대적 위치 선정을 위한 중간값
+            int element = 0; 
 
             //뽑아내려고 하는 원소의 위치가 중간 원소의 위치보다 앞에 있거나 같으면
             if(index <= mid){
