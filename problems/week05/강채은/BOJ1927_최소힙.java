de8f8226f691ba6a41ce7fadc74603cab19f308b
Chaeniiiii <kce2360@gmail.com>
2024-10-17 02:01:57 +0900

    강채은 : BOJ 1927 최소 힙

--- problems/week05/강채은/BOJ1927_최소힙.java
+++ problems/week05/강채은/BOJ1927_최소힙.java
@@ -3,36 +3,28 @@ import java.io.*;
 
 public class BOJ1927_최소힙{
     
-    private static PriorityQueue<Integer> heap;
-
-    private static void addNum(int num){
-
-        heap.add(num);
-
-    }
-
-    private static void removeNum(){
-        
-        if(heap.isEmpty()) System.out.println(0);
-        else System.out.println(heap.poll());
-
-    }
-
     public static void main(String[] args) throws Exception{
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int n = Integer.parseInt(br.readLine());
         
-        heap = new PriorityQueue<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         while(n -- > 0){
             
             int num = Integer.parseInt(br.readLine());
 
-            if(num == 0) removeNum();
-            else addNum(num);
-
+            if(num == 0){
+                if(pq.isEmpty()) {
+                    System.out.println(0);
+                    continue;
+                }
+              System.out.println(pq.poll());  
+              continue;
+            } 
+            pq.add(num);
+            
         }
         
     }
