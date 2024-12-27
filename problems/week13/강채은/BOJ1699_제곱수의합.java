c42480b2139efc41a3a279cd60fb07ec5b81704b
Chaeniiiii <kce2360@gmail.com>
2024-12-27 14:11:56 +0900

    강채은 : BOJ 1699 제곱수의 합

---
+++ problems/week13/강채은/BOJ1699_제곱수의합.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class BOJ1699_제곱수의합 {
+
+    public static void main(String[] args) throws Exception {
+     
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int n = Integer.parseInt(br.readLine());
+        int [] dp = new int[100001];
+
+        for(int i = 1; i<=n ; i++){
+            dp[i] = i;
+        }
+
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j * j <= i; j++){
+                if(dp[i] > dp[i - (j*j)] +1) dp[i] = dp[i-(j*j)] + 1; 
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+
+}
